c357cf6af9a7ae99f0610a98b94e449e296b8816
Mick Pollard <261833+aussielunix@users.noreply.github.com>
2024-01-25 19:03:29 +1100

    removing obsolete script

--- bin/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir $HOME/bin
-curl --silent -fLo $HOME/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $HOME/bin/yadm
-$HOME/bin/yadm clone --bootstrap git@gitlab-01.hl.valueline.io:aussielunix/dotfiles.git
-$HOME/bin/yadm checkout $HOME
-echo "Your dotfiles are in place."
-echo "Logout and back in and then run 'yadm decrypt'"
