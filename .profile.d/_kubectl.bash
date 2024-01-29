38f74e8ab6412355baddf2dd539a519c371269f4
Mick Pollard <261833+aussielunix@users.noreply.github.com>
2024-01-30 08:01:04 +1100

    stripped some obsolete stuff out

--- .profile.d/_kubectl.bash
+++ .profile.d/_kubectl.bash
@@ -1,18 +1,11 @@
 
-source <(~/bin/kubectl completion bash)
-
 alias k='kubectl '
 complete -o default -F __start_kubectl k
-
-
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-source <(~/bin/kubectl krew completion bash)
 
 function kgall {
   for i in $(kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort | uniq); do
     echo "Resource:" $i
     kubectl get --ignore-not-found ${i} -A
-    #kubectl -n monitoring get --ignore-not-found ${i}
   done
 }
 
